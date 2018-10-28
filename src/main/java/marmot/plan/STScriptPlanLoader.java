fcf32abbdcafd5283d4d562615bd932b677200f6
kwlee <kwlee@etri.re.kr>
2018-10-28 15:34:04 +0900

    StringTemplate에 Mvel template 기능 추가

--- src/main/java/marmot/plan/STScriptPlanLoader.java
+++ src/main/java/marmot/plan/STScriptPlanLoader.java
@@ -5,11 +5,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.Map;
 
+import org.mvel2.templates.TemplateRuntime;
 import org.stringtemplate.v4.ST;
 import org.stringtemplate.v4.STGroup;
 import org.stringtemplate.v4.STGroupFile;
 
+import com.google.common.collect.Maps;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import io.vavr.Lazy;
@@ -21,6 +24,7 @@ import utils.io.IOUtils;
  * @author Kang-Woo Lee (ETRI)
  */
 public class STScriptPlanLoader {
+	private static final Map VARS = Maps.newHashMap();
 	private static final String TEMPLATE_GROUP_RESOURCE = "marmot/plan/operator_templates.stg";
 	private static Lazy<STGroup> s_tmpltGroup = Lazy.of(STScriptPlanLoader::loadTemplateGroup);
 	
@@ -52,6 +56,9 @@ public class STScriptPlanLoader {
 	}
 	
 	public static String toJson(String script) {
+		// MVEL template 엔진을 써서 1차 변환함. 
+		script = (String)TemplateRuntime.eval(script, VARS);
+		
 		return new ST(s_tmpltGroup.get(), script).render();
 	}
 	
