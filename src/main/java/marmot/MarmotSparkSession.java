54a07d247e0f76b9463d9e59be0b08f1dbaceb26
kwlee <kwlee@node95.sbdata.etri.re.kr>
2019-11-26 21:28:53 +0900

    method 추가

--- src/main/java/marmot/MarmotSparkSession.java
+++ src/main/java/marmot/MarmotSparkSession.java
@@ -7,8 +7,13 @@ import marmot.exec.MarmotExecutionException;
  * @author Kang-Woo Lee (ETRI)
  */
 public interface MarmotSparkSession {
-	public void createOrReplaceView(String viewName, String dsId);
+	public void execute(Plan plan, ExecutePlanOptions opts)
+		throws MarmotExecutionException;
+	public default void execute(Plan plan) throws MarmotExecutionException {
+		execute(plan, ExecutePlanOptions.DEFAULT);
+	}
 	
+	public void createOrReplaceView(String viewName, String dsId);
 	public void runSql(String sqlStmt, String outDsId, StoreDataSetOptions opts)
 		throws MarmotExecutionException;
 }
